e48586e4af5f908495570f054f319188169c4c8e
Francisco Calaça <chicocx@gmail.com>
2017-11-18 14:16:26 -0200

    first commit

---
+++ bigdataiot2.ino
@@ -0,0 +1,30 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h> //5.7.0
+
+const char* ssid     = "IoTcel";
+const char* password = "12348765";
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  pinMode(2, OUTPUT);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
