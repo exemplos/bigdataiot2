4df2ab014dae06f527477d0a097c3598b9e1ac08
Francisco Calaça <chicocx@gmail.com>
2017-11-18 15:41:58 -0200

    first commit

--- bigdataiot2.ino
+++ bigdataiot2.ino
@@ -2,9 +2,13 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h> //5.7.0
 
-const char* ssid     = "IoTcel";
+const char* ssid     = "iot-mobile";
 const char* password = "12348765";
 
+unsigned long lastTime = 0;
+
+WiFiClient CLIENT;
+PubSubClient MQTT(CLIENT);
 
 void setup() {
   Serial.begin(115200);
@@ -23,10 +27,61 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nConectado!\n\n");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  MQTT.setServer("192.168.8.101", 1883);
+  MQTT.setCallback(callback);
+
 }
 
+void callback(char* topic, byte* payload, unsigned int length){
+  DynamicJsonBuffer jsonBuffer;
+  String json = (char*)payload;
+  JsonObject& rootRead = jsonBuffer.parseObject(json);
+  long s = rootRead["status"];
+  long i = rootRead["id"];
+  Serial.println(json);
+  if(s == 1){
+    digitalWrite(2, HIGH);
+    Serial.println("...ligar");
+  }else{
+    digitalWrite(2, LOW);
+    Serial.println("...desligar");
+  }
+}
+
+void reconnect() {
+  while (!MQTT.connected()) {
+    if (MQTT.connect("ESP8266-Francisco-Aula")) {
+      MQTT.subscribe("aula/bigdata");
+    } else {
+      Serial.print(".");
+      delay(3000);
+    }
+  }
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  reconnect();
+  MQTT.loop();
+  unsigned long now = millis();
+  if((lastTime + 5000) < now){
+    lastTime = now;
+    
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    root["temp"] = 20;
+    root["humid"] = 70;
+    root["time"] = 123;
+    root["id"] = "Francisco";
+    String msg;
+    root.printTo(msg);
+    Serial.println(msg);
+    MQTT.publish("temp/real", msg.c_str());
+    
+  }
 
 }
